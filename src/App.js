ea94aa47a31b50866e256c7416b1dea2ab6d263a
khauminhduy <khauminhduy@gmail.com>
2022-10-19 14:52:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "antd/dist/antd.css";
+import { Button } from "antd";
+
+function Button1() {
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Button1></Button1>
+    </>
   );
 }
 
